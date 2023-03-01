3ddc1bb1fd59f29023d63b931c3395a99a1de051
thomas-mescall <122935621+thomas-mescall@users.noreply.github.com>
2023-03-01 13:58:37 -0600

    practice time with the coffee project

--- js/practice_folder.js
+++ js/practice_folder.js
@@ -1,39 +1,65 @@
-// // The primary purpose of this file is to practice code used in the textbook
-//
-// var todoList = ['Take out the trash', 'Clean the car', 'Pay the bills'];
-//
-// console.log('My todo list:');
-// console.log(todoList);
-// // ['Take out the trash', 'Clean the car', 'Pay the bills']
-//
-// console.log('Completing the last item: ' + todoList[todoList.length - 1]);
-//
-// // let's remove the last item
-// var removedItem = todoList.pop();
-//
-// // log what we did
-// console.log('Task complete: ' + removedItem);
-//
-// console.log(todoList);
-// // ['Take out the trash', 'Clean the car']
-//
-// console.log('Completing the first item: ' + todoList[0]);
-//
-// // let's remove the first item
-// var doneItem = todoList.shift();
-//
-// // log what we did
-// console.log('Task complete: ' + doneItem);
-//
-// console.log(todoList);
-// // ['Clean the car']
+"use strict"
 
+function renderCoffee(coffee) {
+    var html = '<tr class="coffee">';
+    html += '<td>' + coffee.id + '</td>';
+    html += '<td>' + coffee.name + '</td>';
+    html += '<td>' + coffee.roast + '</td>';
+    html += '</tr>';
 
-var namesString = "Joe,Bob,Sally";
+    return html;
+}
 
-console.log(namesString);
-// Joe,Bob,Sally
+function renderCoffees(coffees) {
+    var html = '';
+    for(var i = coffees.length - 1; i >= 0; i--) {
+        html += renderCoffee(coffees[i]);
+    }
+    return html;
+}
 
-var namesArray = namesString.split(',');
+function updateCoffees(e) {
+    e.preventDefault(); // don't submit the form, we just want to update the data
+    var selectedRoast = roastSelection.value;
+    var filteredCoffees = [];
+    coffees.forEach(function(coffee) {
+        if (coffee.roast === selectedRoast) {
+            filteredCoffees.push(coffee);
+        }
+    });
+    tbody.innerHTML = renderCoffees(filteredCoffees);
+}
 
-console.log(namesArray);
+// from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
+var coffees = [
+    {id: 1, name: 'Light City', roast: 'light'},
+    {id: 2, name: 'Half City', roast: 'light'},
+    {id: 3, name: 'Cinnamon', roast: 'light'},
+    {id: 4, name: 'City', roast: 'medium'},
+    {id: 5, name: 'American', roast: 'medium'},
+    {id: 6, name: 'Breakfast', roast: 'medium'},
+    {id: 7, name: 'High', roast: 'dark'},
+    {id: 8, name: 'Continental', roast: 'dark'},
+    {id: 9, name: 'New Orleans', roast: 'dark'},
+    {id: 10, name: 'European', roast: 'dark'},
+    {id: 11, name: 'Espresso', roast: 'dark'},
+    {id: 12, name: 'Viennese', roast: 'dark'},
+    {id: 13, name: 'Italian', roast: 'dark'},
+    {id: 14, name: 'French', roast: 'dark'},
+];
+
+var tbody = document.querySelector('#coffees');
+var submitButton = document.querySelector('#submit');
+var roastSelection = document.querySelector('#roast-selection');
+
+tbody.innerHTML = renderCoffees(coffees);
+
+submitButton.addEventListener('click', updateCoffees);
+
+function nameScanner(x) {
+    for (var i = 0, i < coffees.name.length, i++) {
+        if (i == 'a' || 'A') {
+            // TODO: insert something to connect loop to input
+        }
+    }
+}
