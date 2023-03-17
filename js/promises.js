7a0e7613bce63f7b5ee7750e9642b65bccdf92bb
thomas-mescall <122935621+thomas-mescall@users.noreply.github.com>
2023-03-17 16:54:51 -0500

    getting started on promises

---
+++ js/promises.js
@@ -0,0 +1,6 @@
+"use strict";
+// Create a function that accepts a GitHub username, and returns a promise that resolves returning just the date of the last commit that user made. Reference the github api documentation to achieve this.
+
+let myPromise = fetch(url, {headers: {'Authorization': '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'}
+}
+)
